328a125ffea79fe8452d5827e23f70cef9f5b4e1
Takafumi ONAKA <takafumi.onaka@gmail.com>
2020-11-26 01:28:35 +0900

    Use inline templates

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ class App < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+  enable :inline_templates
 
   get "/" do
     if params[:url] && !params[:url].empty?
@@ -12,3 +13,17 @@ class App < Sinatra::Base
     slim :index
   end
 end
+
+__END__
+
+@@ index
+doctype html
+html
+  head
+    title Qrcode
+  body
+    form[action="/" method="GET"]
+      input[type="text" name="url" size="60"]
+      input[type="submit" value="QRコード生成"]
+    - if @qr
+      == @qr.as_svg
